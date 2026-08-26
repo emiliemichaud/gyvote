f8bba3e3cef56e0e8fa356c69e733b6105697563
Emilie Michaud <85625791+emiliemichaud@users.noreply.github.com>
2026-08-26 21:13:06 +0200

    Update supabase-config.js
    Modification de l'URL

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@
 // À REMPLIR : coordonnées de ton projet Supabase
 // Dashboard Supabase > Project Settings > API
 // ============================================================
-window.SUPABASE_URL = "https://mhtowhgdyutjytyachqe.supabase.co/rest/v1/";
+window.SUPABASE_URL = "https://mhtowhgdyutjytyachqe.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_hYR4M-xfugavv-tTTGZxNA_n7ojHhF7";
